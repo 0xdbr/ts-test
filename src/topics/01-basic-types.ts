57fcaeea35544b900b06ed6cacf040201d722119
diego borja <diegoborjarivas@gmail.com>
2024-07-11 14:43:44 +0200

    Initial commit: Getting into Typescript.~

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,24 @@
+// nummber, strings, boolean, date, any.
+
+// #region NUMBER
+console.log('****************');
+console.log('*****NUMBER*****');
+console.log('****************');
+
+const age = 15;
+let weight = 70.5;
+let weight2:number;
+
+weight = 85;
+weight2 = 74.5;
+
+console.log(age);
+console.log(weight);
+console.log(weight2);
+
+const ageText = '31;
+console.log('parseInt => ', parseInt(ageText));
+
+//#endregion
+
+export{};
